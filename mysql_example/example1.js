e5a3a83d8d824a31f0eb3fa65272ec632cdf0537
Nonani <cjw1234577@gmail.com>
2020-10-10 21:14:57 +0900

    mysql-node.js 연결 예제

--- mysql_example/example1.js
+++ mysql_example/example1.js
@@ -18,16 +18,3 @@ conn.query('SELECT * FROM author', function(err, rows, fields) {
   
   });
   conn.end();
-
-
-// module.exports = {
-//     init: function () {
-//         return mysql.createConnection(db_info);
-//     },
-//     connect: function(conn) {
-//         conn.connect(function(err) {
-//             if(err) console.error('mysql connection error : ' + err);
-//             else console.log('mysql is connected successfully!');
-//         });
-//     }
-// }

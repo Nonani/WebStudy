996d96ae81cd7c2c149f7bece6c8a11a2c961f23
Nonani <cjw1234577@gmail.com>
2020-11-05 22:55:53 +0900

    add auth router

--- generator-example/routes/index.js
+++ generator-example/routes/index.js
@@ -1,27 +1,11 @@
 var express = require('express');
 var router = express.Router();
-var db = require('../config/conn_database.js');
-var conn = db.init();
-db.connect(conn);   //db 연결
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/sign_in', function(req, res){
-
-  var sql = 'SELECT * FROM user where id = ? and pwd = ?'
-      
-  conn.query(sql, [req.body.id, req.body.pwd], function(err, result, field){
-      if(err) throw err;
-      
-      if(result.length===1){  
-          res.render('index', { title: 'Login Success!' });
-      }else{
-          res.render('index', { title: 'Login Fail!' });
-      }
-  });
-});
 
 module.exports = router;

a536bd00b587a335c338aa64989909624903e1dc
Nonani <cjw1234577@gmail.com>
2020-11-05 22:16:48 +0900

    로그인 기능 구현

---
+++ generator-example/config/conn_database.js
@@ -0,0 +1,25 @@
+var mysql = require('mysql');
+var db_info = {
+    host: 'localhost',
+    port: '5306',
+    user: 'root',
+    password: '1234',
+    database: 'example'
+}
+module.exports={
+  init: function(){
+    return mysql.createConnection(db_info);
+  },
+  connect: function(conn){
+    conn.connect(function(err){
+      if(err) console.error('connect fail : '+ err);
+      else console.log('connected successfully!');
+    });
+  },
+  end: function(){
+    console.log('db 연결 종료!')
+    conn.end();
+  }
+}
+
+  
--- generator-example/routes/users.js
+++ generator-example/routes/users.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
+// var db = require('../config/conn_database.js');
+// var conn = db.init();
+// db.connect(conn);   //db 연결
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+// router.post('/sign_in', function(req, res){
+
+//   var sql = 'SELECT * FROM user where id = ? and pwd = ?'
+      
+//   conn.query(sql, [req.body.id, req.body.pwd], function(err, result, field){
+//       if(err) throw err;
+      
+//       if(result.length===1){  
+//           res.render('index', { title: 'Login Success!' });
+//       }else{
+//           res.render('index', { title: 'Login Fail!' });
+//       }
+//   });
+// });
 
 module.exports = router;
